1de9fc00e262b689ae3f48956aa1e98b46acd11d
Daniel Dao <dannydao12345@gmail.com>
2023-05-12 00:23:33 -0700

    Worked on CSV

--- HashTables and File Reading.zip_expanded/Maps and File Reading/src/MapsAndScanner.java
+++ HashTables and File Reading.zip_expanded/Maps and File Reading/src/MapsAndScanner.java
@@ -7,209 +7,94 @@ public class MapsAndScanner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		try {
-			Scanner s = new Scanner(new File("covid317.csv"));
-			int count = 0;
-			s.nextLine(); //skips the column headers!
+			File f = new File("covid317.csv");
+			Scanner s = new Scanner(f);
+			int CAcount = 0;
+			s.nextLine();// skips the column headers!
+			String[][] store = new String[20781][15]; // split it by rows and columns of information
+			int row = 0;
+			while (s.hasNextLine()) {
+				String g = s.nextLine();
+				System.out.println(g);
+				store[row] = g.split(",");
+				row++;
+			}
+			for(int r = 0; r < store.length; r++) {
+				for(int c = 0; c < store[0].length; c++) {
+					if(store[r][c] == "CA") {
+						CAcount++;
+					}
+				}
+			}
 			
 			HashMap<String, Integer> states = new HashMap<String, Integer>();
-			
-			while(s.hasNextLine()) {
-				System.out.println(s);
-			}
-	
-			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
 	}
 
 }
 
 class Animate {
-    public static void main(String[] args) throws InterruptedException {
-        String[] frames = {
-                "   _____            _       _        ",
-                "  / ____|          (_)     | |       ",
-                " | |     ___  _ __  _ _ __ | |_ ___  ",
-                " | |    / _ \\| '_ \\| | '_ \\| __/ _ \\ ",
-                " | |___| (_) | | | | | |_) | || (_) |",
-                "  \\_____\\___/|_| |_|_| .__/ \\__\\___/ ",
-                "                     | |             ",
-                "                     |_|             ",
-                "",
-                "",
-                "We're no strangers to love",
-                "You know the rules and so do I",
-                "A full commitment's what I'm thinking of",
-                "You wouldn't get this from any other guy",
-                "",
-                "I just wanna tell you how I'm feeling",
-                "Gotta make you understand",
-                "",
-                "Never gonna give you up",
-                "Never gonna let you down",
-                "Never gonna run around and desert you",
-                "Never gonna make you cry",
-                "Never gonna say goodbye",
-                "Never gonna tell a lie and hurt you",
-                "",
-                "We've known each other for so long",
-                "Your heart's been aching but",
-                "You're too shy to say it",
-                "Inside we both know what's been going on",
-                "We know the game and we're gonna play it",
-                "",
-                "And if you ask me how I'm feeling",
-                "Don't tell me you're too blind to see",
-                "",
-                "Never gonna give you up",
-                "Never gonna let you down",
-                "Never gonna run around and desert you",
-                "Never gonna make you cry",
-                "Never gonna say goodbye",
-                "Never gonna tell a lie and hurt you",
-                "",
-                "Never gonna give you up",
-                "Never gonna let you down",
-                "Never gonna run around and desert you",
-                "Never gonna make you cry",
-                "Never gonna say goodbye",
-                "Never gonna tell a lie and hurt you",
-                "",
-                "Give you up, give you up",
-                "Never gonna give you up",
-                "Give you up, give you up",
-                "Never gonna give you up",
-                "",
-                "We've known each other for so long",
-                "Your heart's been aching but",
-                "You're too shy to say it",
-                "Inside we both know what's been going on",
-                "We know the game and we're gonna play it",
-                "",
-                "I just wanna tell you how I'm feeling",
-                "Gotta make you understand",
-                "",
-                "Never gonna give you up",
-                "Never gonna let you down",
-                "Never gonna run around and desert you",
-                "Never gonna make you cry",
-                "Never gonna say goodbye",
-                "Never gonna tell a lie and hurt you",
-                "",
-                "Never gonna give you up",
-                "Never gonna let you down",
-                "Never gonna run around and desert you",
-                "Never gonna make you cry",
-                "Never gonna say goodbye",
-                "Never gonna tell a lie and hurt you",
-                "",
-                "Give you up, give you up",
-                "Never gonna give you up",
-                "Give you up, give you up",
-                "Never gonna give you up",
-        };
-        
-        int frameDelay = 200; // milliseconds between frames
-    }
-    
-    static String[] frames = {
-            "   _____            _       _        ",
-            "  / ____|          (_)     | |       ",
-            " | |     ___  _ __  _ _ __ | |_ ___  ",
-            " | |    / _ \\| '_ \\| | '_ \\| __/ _ \\ ",
-            " | |___| (_) | | | | | |_) | || (_) |",
-            "  \\_____\\___/|_| |_|_| .__/ \\__\\___/ ",
-            "                     | |             ",
-            "                     |_|             ",
-            "",
-            "",
-            "We're no strangers to love",
-            "You know the rules and so do I",
-            "A full commitment's what I'm thinking of",
-            "You wouldn't get this from any other guy",
-            "",
-            "I just wanna tell you how I'm feeling",
-            "Gotta make you understand",
-            "",
-            "Never gonna give you up",
-            "Never gonna let you down",
-            "Never gonna run around and desert you",
-            "Never gonna make you cry",
-            "Never gonna say goodbye",
-            "Never gonna tell a lie and hurt you",
-            "",
-            "We've known each other for so long",
-            "Your heart's been aching but",
-            "You're too shy to say it",
-            "Inside we both know what's been going on",
-            "We know the game and we're gonna play it",
-            "",
-            "And if you ask me how I'm feeling",
-            "Don't tell me you're too blind to see",
-            "",
-            "Never gonna give you up",
-            "Never gonna let you down",
-            "Never gonna run around and desert you",
-            "Never gonna make you cry",
-            "Never gonna say goodbye",
-            "Never gonna tell a lie and hurt you",
-            "",
-            "Never gonna give you up",
-            "Never gonna let you down",
-            "Never gonna run around and desert you",
-            "Never gonna make you cry",
-            "Never gonna say goodbye",
-            "Never gonna tell a lie and hurt you",
-            "",
-            "Give you up, give you up",
-            "Never gonna give you up",
-            "Give you up, give you up",
-            "Never gonna give you up",
-            "",
-            "We've known each other for so long",
-            "Your heart's been aching but",
-            "You're too shy to say it",
-            "Inside we both know what's been going on",
-            "We know the game and we're gonna play it",
-            "",
-            "I just wanna tell you how I'm feeling",
-            "Gotta make you understand",
-            "",
-            "Never gonna give you up",
-            "Never gonna let you down",
-            "Never gonna run around and desert you",
-            "Never gonna make you cry",
-            "Never gonna say goodbye",
-            "Never gonna tell a lie and hurt you",
-            "",
-            "Never gonna give you up",
-            "Never gonna let you down",
-            "Never gonna run around and desert you",
-            "Never gonna make you cry",
-            "Never gonna say goodbye",
-            "Never gonna tell a lie and hurt you",
-            "",
-            "Give you up, give you up",
-            "Never gonna give you up",
-            "Give you up, give you up",
-            "Never gonna give you up",
-    };
-    
+	public static void main(String[] args) throws InterruptedException {
+		String[] frames = { "   _____            _       _        ", "  / ____|          (_)     | |       ",
+				" | |     ___  _ __  _ _ __ | |_ ___  ", " | |    / _ \\| '_ \\| | '_ \\| __/ _ \\ ",
+				" | |___| (_) | | | | | |_) | || (_) |", "  \\_____\\___/|_| |_|_| .__/ \\__\\___/ ",
+				"                     | |             ", "                     |_|             ", "", "",
+				"We're no strangers to love", "You know the rules and so do I",
+				"A full commitment's what I'm thinking of", "You wouldn't get this from any other guy", "",
+				"I just wanna tell you how I'm feeling", "Gotta make you understand", "", "Never gonna give you up",
+				"Never gonna let you down", "Never gonna run around and desert you", "Never gonna make you cry",
+				"Never gonna say goodbye", "Never gonna tell a lie and hurt you", "",
+				"We've known each other for so long", "Your heart's been aching but", "You're too shy to say it",
+				"Inside we both know what's been going on", "We know the game and we're gonna play it", "",
+				"And if you ask me how I'm feeling", "Don't tell me you're too blind to see", "",
+				"Never gonna give you up", "Never gonna let you down", "Never gonna run around and desert you",
+				"Never gonna make you cry", "Never gonna say goodbye", "Never gonna tell a lie and hurt you", "",
+				"Never gonna give you up", "Never gonna let you down", "Never gonna run around and desert you",
+				"Never gonna make you cry", "Never gonna say goodbye", "Never gonna tell a lie and hurt you", "",
+				"Give you up, give you up", "Never gonna give you up", "Give you up, give you up",
+				"Never gonna give you up", "", "We've known each other for so long", "Your heart's been aching but",
+				"You're too shy to say it", "Inside we both know what's been going on",
+				"We know the game and we're gonna play it", "", "I just wanna tell you how I'm feeling",
+				"Gotta make you understand", "", "Never gonna give you up", "Never gonna let you down",
+				"Never gonna run around and desert you", "Never gonna make you cry", "Never gonna say goodbye",
+				"Never gonna tell a lie and hurt you", "", "Never gonna give you up", "Never gonna let you down",
+				"Never gonna run around and desert you", "Never gonna make you cry", "Never gonna say goodbye",
+				"Never gonna tell a lie and hurt you", "", "Give you up, give you up", "Never gonna give you up",
+				"Give you up, give you up", "Never gonna give you up", };
+
+		int frameDelay = 200; // milliseconds between frames
+	}
+
+	static String[] frames = { "   _____            _       _        ", "  / ____|          (_)     | |       ",
+			" | |     ___  _ __  _ _ __ | |_ ___  ", " | |    / _ \\| '_ \\| | '_ \\| __/ _ \\ ",
+			" | |___| (_) | | | | | |_) | || (_) |", "  \\_____\\___/|_| |_|_| .__/ \\__\\___/ ",
+			"                     | |             ", "                     |_|             ", "", "",
+			"We're no strangers to love", "You know the rules and so do I", "A full commitment's what I'm thinking of",
+			"You wouldn't get this from any other guy", "", "I just wanna tell you how I'm feeling",
+			"Gotta make you understand", "", "Never gonna give you up", "Never gonna let you down",
+			"Never gonna run around and desert you", "Never gonna make you cry", "Never gonna say goodbye",
+			"Never gonna tell a lie and hurt you", "", "We've known each other for so long",
+			"Your heart's been aching but", "You're too shy to say it", "Inside we both know what's been going on",
+			"We know the game and we're gonna play it", "", "And if you ask me how I'm feeling",
+			"Don't tell me you're too blind to see", "", "Never gonna give you up", "Never gonna let you down",
+			"Never gonna run around and desert you", "Never gonna make you cry", "Never gonna say goodbye",
+			"Never gonna tell a lie and hurt you", "", "Never gonna give you up", "Never gonna let you down",
+			"Never gonna run around and desert you", "Never gonna make you cry", "Never gonna say goodbye",
+			"Never gonna tell a lie and hurt you", "", "Give you up, give you up", "Never gonna give you up",
+			"Give you up, give you up", "Never gonna give you up", "", "We've known each other for so long",
+			"Your heart's been aching but", "You're too shy to say it", "Inside we both know what's been going on",
+			"We know the game and we're gonna play it", "", "I just wanna tell you how I'm feeling",
+			"Gotta make you understand", "", "Never gonna give you up", "Never gonna let you down",
+			"Never gonna run around and desert you", "Never gonna make you cry", "Never gonna say goodbye",
+			"Never gonna tell a lie and hurt you", "", "Never gonna give you up", "Never gonna let you down",
+			"Never gonna run around and desert you", "Never gonna make you cry", "Never gonna say goodbye",
+			"Never gonna tell a lie and hurt you", "", "Give you up, give you up", "Never gonna give you up",
+			"Give you up, give you up", "Never gonna give you up", };
+
 }
